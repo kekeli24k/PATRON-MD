4841eba37a10fe363946c2f13455d1530bab418f
Kekeli <163278311+kekeli24k@users.noreply.github.com>
2025-01-23 07:14:41 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "KEKELI24"
 
-global.banknumber = "8133729715"
+global.banknumber = "233242848862"
 
-global.bankowner = "FADARE"
+global.bankowner = "KEKELI"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
